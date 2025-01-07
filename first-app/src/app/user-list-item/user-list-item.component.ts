fe17595127dd8897388639c86b88ee7c8ea24cac
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-01-07 22:22:42 +0100

    added two new components user-list and user-list-item

---
+++ first-app/src/app/user-list-item/user-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list-item',
+  imports: [],
+  templateUrl: './user-list-item.component.html',
+  styleUrl: './user-list-item.component.scss'
+})
+export class UserListItemComponent {
+
+}
