a2d0f21c4d379d437819440fbd64961b42217fb8
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-01-19 17:08:31 +0100

    added sinchronous Promise

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from "./user-list/user-list.component";
-import { IUser } from './interfaces/user';
 import { UserService } from './user.service';
-
-const p = new Promise((resolve, reject) => {
+import { of } from 'rxjs';
+/* const p = new Promise((resolve, reject) => {
   setTimeout(() => 
     resolve('Hi, How are you?'), 2000);
 });
 
-p.then((value) => console.log(value));
+p.then((value) => console.log(value)); */
+
+Promise.resolve(1000).then(x => x + 1).then(console.log);
 
 
 @Component({
