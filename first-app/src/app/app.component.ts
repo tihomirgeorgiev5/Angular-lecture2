185a508f6a52229089a86ae4487bd22aec975267
Tihomir Georgiev <georgiev.tm@abv.bg>
2025-01-19 17:19:39 +0100

    added equivalent of Promise (who was asynchronous, not synchronous as I say before)

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { UserListComponent } from "./user-list/user-list.component";
 import { UserService } from './user.service';
 import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 /* const p = new Promise((resolve, reject) => {
   setTimeout(() => 
     resolve('Hi, How are you?'), 2000);
 });
 
 p.then((value) => console.log(value)); */
+[1000, 200, 300].map(x => x + 100).map(console.log);
 
 Promise.resolve(1000).then(x => x + 1).then(console.log);
+
+of(1000, 200, 300).pipe(
+  map(x => x + 100),
+).subscribe((x) => {console.log(x);
+});
 
 
 @Component({
